9005c7feec0fbe3ae0fda1a82edfb10d5f15cbc1
Thamizhmaran-k <thamizhmaran0@gmail.com>
2025-08-14 08:51:26 +0530

    AgeCalculator.java

--- AgeCalculator.java
+++ AgeCalculator.java
@@ -4,29 +4,29 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class AgeCalculator {
-	public static void main(String args[]) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter your birthdate ( yyy-mm-dd):");
-		String input = scan.nextLine();
-		
-		LocalDate birthDate = LocalDate.parse(input);
-		LocalDate currentDate = LocalDate.now();
-		
-		int years = currentDate.getYear() -birthDate.getYear();
-		int months = currentDate.getMonthValue() -birthDate.getMonthValue();
-		int days = currentDate.getDayOfMonth() -birthDate.getDayOfMonth();
-		
-		if(days < 0) {
-			months--;
-			days += birthDate.plusMonths(months + years * 12).lengthOfMonth();
-		}
-		if(months < 0) {
-			years--;
-			months += 12;
-		}
-	
-		System.out.printf("Your age is: %d years, %d months, and %d days.%n",  years, months, days);
-		scan.close();
+	    public static void main(String args[]) {
+	        Scanner scan = new Scanner(System.in);
+	        System.out.print("Enter your birthdate (yyyy-mm-dd): ");
+	        String birthInput = scan.nextLine();
+	        LocalDate birthDate = LocalDate.parse(birthInput);
+	        LocalDate currentDate = LocalDate.of(2023, 9, 28);
+	        
+	        int years = currentDate.getYear() - birthDate.getYear();
+	        int months = currentDate.getMonthValue() - birthDate.getMonthValue();
+	        int days = currentDate.getDayOfMonth() - birthDate.getDayOfMonth();
+
+	        if(days < 0) {
+	            months--;
+	            days += birthDate.plusMonths(months + years * 12).lengthOfMonth();
+	        }
+	        if(months < 0) {
+	            years--;
+	            months += 12;
+	        }
+	        System.out.printf("Your age is: %d years, %d months, and %d days.%n",
+	                years, months, days);
+
+	        scan.close();
+	    }
 	}
 
-}
